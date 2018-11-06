8034651f5ac0fc2202b53b76d726e8592bfe78d0
hananana <hananana.vimmer@gmail.com>
2018-11-06 15:15:35 +0900

    :hammer: add inspix

---
+++ template/cs/inspix.cs
@@ -0,0 +1,12 @@
+using System.Collection;
+using System.Collection.Generics;
+using System.Linq;
+using UnityEngine;
+ 
+namespace Ignis
+{
+    public class {{_name_}}
+    {
+        {{_cursor_}}
+    }
+}
